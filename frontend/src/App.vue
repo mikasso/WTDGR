91df38656af4e6a0b142a56449524c8a58ca99ce
Piokor <piokor123@gmail.com>
2021-02-24 17:23:10 +0100

    Toolbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainPanel msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 
-import HelloWorld from './components/HelloWorld.vue'
+import MainPanel from './components/MainPanel.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPanel
   }
 }
 </script>
 
 <style>
+html, body{
+  min-height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ frontend/src/components/MainPanel.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="main-panel">
+      <div class='toolbar'>
+          <div class='menu'>
+              menu
+          </div>
+          <div class='tools'>
+            <div 
+            v-for='(tool, index) in toolbar.tools' v-bind:key='index' 
+            class='tool' :class="{selected: tool.name == toolbar.selected_tool}"
+            @click='toolbar.selected_tool = tool.name'>
+                <img :src="tool.img ? require('../assets/' + tool.img + '.png') : ''">
+                {{tool.name}}
+            </div>
+          </div>
+      </div>
+
+      <div class='table'>
+          tablica
+      </div>
+
+      <div class='users'>
+          userzy
+      </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: 'MainPanel',
+    mounted(){
+
+    },
+    
+    data: ()=> ({
+        toolbar: {
+            selected_tool: 'Select',
+            tools: [
+                {
+                    name: 'Select',
+                    img: 'select',
+                },
+                {
+                    name: 'Vertex',
+                    img: 'vertex',
+                },
+                {
+                    name: 'Edge',
+                    img: 'edge',
+                },
+                {
+                    name: 'Path',
+                    img: 'path',
+                },
+                {
+                    name: 'Star',
+                    img: 'star',
+                },
+                {
+                    name: 'Erase',
+                    img: 'earse',
+                },
+                {
+                    name: 'Label',
+                    img: 'label',
+                },
+            ]
+        }
+    }),
+
+    methods: {
+        getImgUrl(pic) {
+            return require(`../assets/'${pic}.png`)
+        }
+    }
+
+}
+</script>
+
+
+<style scoped lang='scss'>
+    .main-panel{
+        width: 100%;
+        min-height: 100%;
+
+        display: inline-grid;
+        grid-template-columns: auto 20%;
+        grid-template-rows: 8vh auto;
+
+        .toolbar{
+            grid-column-start: 1;
+            grid-column-end: 1;
+            grid-row-start: 1;            
+            grid-row-end: 1;     
+
+            height: 8vh;
+            
+            .menu{
+                width: 100%;
+                height: 30%;
+            }
+            .tools{
+                width: 100%;
+                height: 50%;
+
+                padding: 5px;
+
+                display: flex;
+                justify-content: flex-start;
+
+                .tool{
+                    margin-right: 5px;
+                    min-width: 50px;
+                    min-height: 25px;
+                    min-height: 25px;
+                    padding: 0px 10px;
+
+                    cursor: pointer;
+
+                    border: rgb(210, 210, 210) 2px solid;
+                    border-radius: 5px;
+
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+
+                    img{
+                        max-height: 22px;
+                        max-width: 22px;
+
+                        margin-right: 5px;
+                    }
+                }
+
+                .selected{
+                    background: rgb(220, 220, 220);
+                }
+            }
+        }
+
+        .table{
+            grid-column-start: 1;
+            grid-column-end: 1;
+            grid-row-start: 2;            
+            grid-row-end: 2;       
+        }
+
+        .users{
+            grid-column-start: 2;
+            grid-column-end: 2;
+            grid-row-start: 1;
+            grid-row-end: span 2;
+        }
+    }
+</style>
