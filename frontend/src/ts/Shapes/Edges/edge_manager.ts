499745e9fb095f6ea9c215599d5fa2030453ff23
Piokor <piokor123@gmail.com>
2021-04-24 13:25:21 +0200

    View repaired Toolbar is working with board Erasing partly done

--- frontend/src/ts/Shapes/Edges/edge_manager.ts
+++ frontend/src/ts/Shapes/Edges/edge_manager.ts
@@ -29,10 +29,12 @@ export class EdgeManager {
   private currentLine: CustomLine | null;
   private currentVertex: Vertex;
   private defaultLineConfig: LineConfig;
+  private edgeCount: number;
   constructor(private layer: Konva.Layer) {
     this.layer = layer;
     this.state = new NotDrawingState();
     this.currentLine = null;
+    this.edgeCount = 0;
     this.defaultLineConfig = {
       points: [],
       stroke: "black",
@@ -43,35 +45,47 @@ export class EdgeManager {
   }
 
   public startDrawing(event: KonvaMouseEvent) {
-    if (!isRightClick(event)) return;
-
     this.state = this.state.startDrawing(() => {
       this.currentVertex = event.target as Vertex;
       const startPoint = this.currentVertex.attrs;
       const endPoint = getPointFromEvent(event);
       const lineConfig = this.defaultLineConfig;
+      lineConfig.name = "edge" + this.edgeCount.toString();
       lineConfig.points = [startPoint.x, startPoint.y, endPoint.x, endPoint.y];
       this.currentLine = new CustomLine(lineConfig);
       this.layer.add(this.currentLine);
-      this.currentLine.draw();
     });
   }
 
   public tryToConnectVertices(event: KonvaMouseEvent) {
-    if (!isRightClick(event)) return;
-
     const v1 = event.currentTarget as Vertex;
     const v2 = this.currentVertex as Vertex;
     const cord = v1.attrs;
     this.currentLine?.redraw(cord);
     const edge = new Edge(v1, v2, this.currentLine!.attrs);
+    this.edgeCount += 1;
     v1.edges.push(edge);
     v2.edges.push(edge);
+    this.layer.add(edge);
+    this.currentLine?.remove();
     this.currentLine = null;
   }
 
   public dragEdges(event: KonvaMouseEvent) {
     const vertex = event.target as Vertex;
+    for (let i = 0; i < vertex.edges.length; i++) {
+      const edge = vertex.edges[i];
+      let toChange = 2;
+      console.log(edge.v1!._id);
+      if (edge.v1!._id !== vertex._id) toChange = 0;
+
+      edge.attrs.points[toChange] = vertex.attrs.x;
+      edge.attrs.points[toChange + 1] = vertex.attrs.y;
+    }
+    this.layer.draw();
+  }
+
+  public dragEdgesOfVertex(vertex: Vertex) {
     for (let i = 0; i < vertex.edges.length; i++) {
       const edge = vertex.edges[i];
       let toChange = 2;
@@ -96,5 +110,26 @@ export class EdgeManager {
       const endPoint = getPointFromEvent(event);
       this.currentLine?.redraw(endPoint);
     });
+  }
+
+  // public removeFromVertex(vertex: Vertex){
+  //   console.log(this.layer.getChildren())
+    
+  //   for(var i = 0; i < vertex.edges.length; i++){
+  //     var edge = vertex.edges[i];
+
+  //     var otherVer = edge.v1;
+  //     if(otherVer?.attrs.name == vertex.attrs.name)
+  //       otherVer = edge.v2;
+
+  //     var removedIndex;
+  //     for(var i = 0; i < otherVer!.edges.length; i++){
+  //       if(otherVer!.edges[i].attrs.name == edge.attrs.name)
+  //         removedIndex = i;
+  //     }
+  //     if(removedIndex)
+  //     otherVer!.edges.splice(removedIndex, 1);
+  //   }  
+    
   }
 }
