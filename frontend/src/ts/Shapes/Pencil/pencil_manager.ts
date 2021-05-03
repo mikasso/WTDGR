ad6f8f43653e200321dd29c6d4bcf0ef17009cde
Piokor <piokor123@gmail.com>
2021-05-03 18:15:20 +0200

    Drawing on the background, without configuration of pencil Erasing the pencil drawing on click

---
+++ frontend/src/ts/Shapes/Pencil/pencil_manager.ts
@@ -0,0 +1,61 @@
+import { KonvaMouseEvent, VertexConfig } from "@/ts/Aliases/aliases";
+import Konva from "konva";
+import { Vector2d } from "konva/types/types";
+import { LineConfig } from "konva/types/shapes/Line";
+
+export class Pencil extends Konva.Line {
+  constructor(config: Konva.LineConfig) {
+    super(config);
+  }
+}
+
+export class PencilManager {
+    public defualtConfig: LineConfig;
+    private drawing: boolean;
+    private currentDrawing: Pencil;
+  
+    constructor(private layer: Konva.Layer) {
+      this.defualtConfig = {
+        points: [],
+        stroke: "red",
+        strokeWidth: 2,
+        lineCap: "round",
+        lineJoin: "round",
+      };
+      this.drawing = false;
+    }
+
+    public create(mousePos: Vector2d | null, config: LineConfig): Pencil {
+        const x = mousePos!.x;
+        const y = mousePos!.y;
+        config.points = [x, y];
+        const pencil: Pencil = new Pencil(config);
+        this.drawing = true;
+        this.currentDrawing = pencil;
+        this.draw();
+        return pencil;
+    }
+
+    public appendPoint(mousePos: Vector2d | null){
+      if(!this.drawing) return;
+      var points = this.currentDrawing.attrs.points;
+      points.push(mousePos!.x);
+      points.push(mousePos!.y);
+      this.currentDrawing.points(points);
+      this.draw();
+    }
+
+    public finishDrawing() {
+      this.drawing = false;
+  }
+
+    public draw() {
+      this.layer.add(this.currentDrawing);
+      this.layer.draw();
+    }      
+
+    public remove(pencilConfig: Pencil){
+      pencilConfig.remove();
+      this.layer.draw();
+    }
+}
