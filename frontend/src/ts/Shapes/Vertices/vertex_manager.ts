9bb0fc705d4653dc467e245d41c105c3294884c4
Piokor <45105555+Piokor@users.noreply.github.com>
2021-06-09 23:04:26 +0200

    Revert "Toolbar typescript correct migration"

--- frontend/src/ts/Shapes/Vertices/vertex_manager.ts
+++ frontend/src/ts/Shapes/Vertices/vertex_manager.ts
@@ -4,11 +4,7 @@ import { Vector2d } from "konva/types/types";
 import { Edge } from "../Edges/edge_manager";
 
 export class Vertex extends Konva.Circle {
-  constructor(
-    config: Konva.CircleConfig, 
-    public layer: Konva.Layer,       
-    public edges: Edge[] = [],
-  ){
+  constructor(config: Konva.CircleConfig, public edges: Edge[] = []) {
     super(config);
   }
 }
@@ -18,9 +14,8 @@ export class VertexManager {
   private dragging: boolean;
   private vertexCount: number;
   private vertexes: Vertex[];
-  public layer: Konva.Layer;
 
-  constructor() {
+  constructor(private layer: Konva.Layer) {
     this.vertexCount = 0;
     this.vertexes = [];
     this.defualtConfig = {
@@ -53,7 +48,7 @@ export class VertexManager {
   public create(config: VertexConfig): Vertex {
     config.name = "Vertex" + this.vertexCount.toString();
     this.vertexCount += 1;
-    const vertex: Vertex = new Vertex(config, this.layer);
+    const vertex: Vertex = new Vertex(config);
     this.vertexes.push(vertex);
     return vertex;
   }
