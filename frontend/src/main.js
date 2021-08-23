268bc1c4bce6553b359283ccf9bd8b66aa9a6a1c
mikasso <mikokaj.szaroleta@gmail.com>
2021-08-23 22:42:18 +0200

    Connection state should is visible in toolbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { ToggleButton } from "vue-js-toggle-button";
+import Vuex from "vuex";
 
+Vue.use(Vuex);
 Vue.component("ToggleButton", ToggleButton);
 Vue.config.productionTip = false;
 Vue.prototype.$config = {
@@ -16,6 +18,27 @@ Vue.prototype.$config = {
 };
 
 Vue.prototype.Konva = require("konva");
+
+const store = new Vuex.Store({
+  state: {
+    isOnline: true,
+  },
+  getters: {
+    isOnline(state) {
+      return state.isOnline;
+    },
+  },
+  mutations: {
+    setOnline(state) {
+      state.isOnline = true;
+    },
+    setOffline(state) {
+      state.isOnline = false;
+    },
+  },
+});
+
 new Vue({
   render: (h) => h(App),
+  store: store,
 }).$mount("#app");
